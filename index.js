e661c5e94e6ac26e5c1081bba77d653ac8cd189a
codesiddhant <siddhantgour1@gmail.com>
2022-03-28 17:48:16 +0530

    sending json to golang

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var socket = require('socket.io-client')('http://edit.webserver.com:4000/');
+var socket = require('socket.io-client')('http://editor.nekione.local:4000/');
 socket.on('connect', function(data){
 });
 
 let i = 0
+
+let data = {
+  "ip" : "12345",
+  "dir": "/home/siddhant" 
+}
+let paw =JSON.stringify(data);
 setInterval(function(){
-  socket.emit("data", "siddhant " + i);
+  socket.emit("data", paw);
  i++
 }, 2000);
 
 socket.on('reply', function(data){
 
-  console.log(data + ' ' + i)
+  console.log(data)
 });
 
 
